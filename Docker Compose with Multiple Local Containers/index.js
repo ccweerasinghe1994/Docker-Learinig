0254d831fd3ca8d74eb9fd27a1eafa46e563fa58
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-03-28 14:04:54 +0530

    compose changes

--- Docker Compose with Multiple Local Containers/index.js
+++ Docker Compose with Multiple Local Containers/index.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const redis = require("redis");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Bi There Bro ");
+const client = redis.createClient({
+  host: "redis-server",
+  port: 6379
 });
-app.listen(8080, () => console.log(`listening on http://localhost:${8080}`));
+client.set("visits", 0);
+
+app.get("*", (req, res) => {
+  client.get("visits", (err, visits) => {
+    res.send("Numebr of Visits " + visits);
+    client.set("visits", parseInt(visits) + 1);
+  });
+});
+
+app.listen(8081, () => {
+  console.log("Listing on port 8081");
+});
